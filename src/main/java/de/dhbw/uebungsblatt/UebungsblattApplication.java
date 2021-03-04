0379ac0825da551fc6e2766c449cf3d4abf879a2
Zoidberg3 <manuel@rodri.de>
2021-03-04 18:13:31 +0100

    Initial commit

---
+++ src/main/java/de/dhbw/uebungsblatt/UebungsblattApplication.java
@@ -0,0 +1,13 @@
+package de.dhbw.uebungsblatt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UebungsblattApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UebungsblattApplication.class, args);
+    }
+
+}
---
+++ src/test/java/de/dhbw/uebungsblatt/UebungsblattApplicationTests.java
@@ -0,0 +1,13 @@
+package de.dhbw.uebungsblatt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UebungsblattApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
